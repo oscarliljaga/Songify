4c5c986e2bb1b20f8993ae872ed2786fafb5a330
oscarliljaga <oscar.lilja@elev.ga.ntig.se>
2024-01-18 10:40:11 +0100

    Uppdaterat diagram

--- src/Release.java
+++ src/Release.java
@@ -3,9 +3,9 @@ import java.util.HashSet;
 public class Release {
     String name;
     String url;
+    ReleaseType type;
     HashSet<Artist> artists;
     HashSet<Song> songs;
-    ReleaseType type;
 
     public Release(String name, String url, HashSet<Artist> artists, HashSet<Song> songs, ReleaseType type) {
         this.name = name;
